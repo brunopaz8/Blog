2c02cda13194a005d0fac4eb9b9187451769eb33
bruno <bp.nascimento12@gmail.com>
2025-03-23 16:04:19 -0300

    criação do menu-title e do big-card

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
+import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
+import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { SmalCardComponent } from "../../components/smal-card/smal-card.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmalCardComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
