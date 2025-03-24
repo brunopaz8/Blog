64c8a409927f44ddc564e6efe9db66a83167549c
bruno <bp.nascimento12@gmail.com>
2025-03-24 11:24:51 -0300

    criando o small-card e o title-card + melhorias

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,11 @@ import { MenuTitleComponent } from "../../components/menu-title/menu-title.compo
 import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmalCardComponent } from "../../components/smal-card/smal-card.component";
+import { CardTitleComponent } from "../../components/card-title/card-title.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmalCardComponent],
+  imports: [MenuTitleComponent, MenuBarComponent, BigCardComponent, SmalCardComponent, CardTitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
