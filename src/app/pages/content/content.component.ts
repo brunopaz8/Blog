d7f65fb2ed3e024e2655d6762b31b0dd5e9ce42b
bruno <bp.nascimento12@gmail.com>
2025-03-25 20:49:14 -0300

    finalizando a estrutura do  Projeto

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
-import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-content',
   imports: [RouterLink],
   templateUrl: './content.component.html',
-  styleUrl: './content.component.css'
+  styleUrls: ['./content.component.css']  
 })
-export class ContentComponent {
-  photoCover:string = ""
-  contentTitle:string = ""
-  contentDescription:string = ""
-  contentowner:string = ""
+export class ContentComponent implements OnInit {
+  photoCover: string = "";
+  contentTitle: string = "";
+  contentDescription: string = "";
+  contentOwner: string = "";
+  private id: string | null = null; 
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.id = params.get('id');
+      this.setValuesToComponent(this.id);
+    });
+  }
+  
+  private setValuesToComponent(id: string | null) {
+    if (!id) return;
+
+    const result = dataFake.find(article => article.id === Number(id));
+
+    if (result) {
+      this.contentTitle = result.title;
+      this.contentDescription = result.description;
+      this.photoCover = result.photo;
+      this.contentOwner = result.owner;
+    }
+  }
 }
+
+
