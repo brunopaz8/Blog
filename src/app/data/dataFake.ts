331d0bedea2730335baed1f91aadd00836126bea
bruno <bp.nascimento12@gmail.com>
2025-03-25 22:53:55 -0300

    hotfix

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,8 +2,16 @@ export const dataFake = [
     {
        "id": 1,
        "photo": "https://preview.redd.it/vl36hkjkzbh91.png?auto=webp&s=b74abae8c544a8fa26f8ef79adb7116c396bde72",
-       "title": "Teste 0",
-       "description": "pipopipipopopopopopopopopo",
-       "owner": "Bruno Prazeres"
-    }
+       "title": "",
+       "description": "",
+       "owner": "Mestre Andre"
+    },
+    {
+        "id": 2,
+        "photo": "https://preview.redd.it/vl36hkjkzbh91.png?auto=webp&s=b74abae8c544a8fa26f8ef79adb7116c396bde72",
+        "title": "la na Loja do mestre Andre",
+        "description": "Foi na loja do Mestre André Que eu comprei um pifarito Tiro, liro, li um pifarito Ai olé, ai olé Foi na loja do Mestre André Ai olé, ai olé Foi na loja do Mestre André Foi na loja do Mestre André Que eu comprei um pianinho Plim plim plim, um pianinho Tiro, liro, li um pifarito Ai olé, ai olé Foi na loja do Mestre André Ai olé, ai olé Foi na loja do Mestre André Foi na loja do Mestre André Que eu comprei um tamborzinho Tum tum tum, um tamborzinho Plim plim plim, um pianinho Tiro, liro, li um pifarito Ai olé, ai olé Foi na loja do Mestre André Ai olé, ai olé Foi na loja do Mestre André Foi na loja do Mestre André Que eu comprei um rabecão Zum, zum, zum, uma rabecão Tum tum tum, um tamborzinho Plim plim plim, um pianinho Tiro, liro, li um pifarito Ai olé, ai olé Foi na loja do Mestre André Ai olé, ai olé Foi na loja do Mestre André Foi na loja do Mestre André Que eu comprei um tamborim Teleco, teco, teco, um tamborim Ai olé, ai olé Foi na loja do Mestre André Ai olé, ai olé Foi na loja do Mestre André Foi na loja do Mestre André Que eu comprei uma sanfona Fom, fom, fom, uma sanfona Teleco, teco, teco, um tamborim Ai olé, ai olé Foi na loja do Mestre André Ai olé, ai olé Foi na loja do Mestre André Tiro, liro, li um pifarito Tum tum tum, um tamborzinho Plim plim plim, um pianinho Zum, zum, zum, uma rabecão Teleco, teco, teco, um tamborim Blam, blam, blam, um violão Fom, fom, fom, uma sanfona Bum, bum, bum, um surdão Chic, chic, chic, um chocalho Rec, rec, rec, um reco-reco Tum, tic, tum, um pandeiro Tcham, tcham, tcham, uma guitarra.",
+        "owner": "Mestre Andre"
+     },
+
 ]
