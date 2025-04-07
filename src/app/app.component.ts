969d815b665a030d9f31e8ffdc464f892906671a
bruno <bp.nascimento12@gmail.com>
2025-04-06 23:39:11 -0300

    adição do menu bottom + interações com os botões

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SmalCardComponent } from './components/smal-card/smal-card.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { HomeComponent } from "./pages/home/home.component";
+import { MenuBottomComponent } from "./components/menu-bottom/menu-bottom.component";
 
 @Component({
   selector: 'app-root',
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { MenuBarComponent } from "../../components/menu-bar/menu-bar.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmalCardComponent } from "../../components/smal-card/smal-card.component";
 import { CardTitleComponent } from "../../components/card-title/card-title.component";
+import { MenuBottomComponent } from "../../components/menu-bottom/menu-bottom.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, BigCardComponent, SmalCardComponent, CardTitleComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmalCardComponent, CardTitleComponent, MenuBottomComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
